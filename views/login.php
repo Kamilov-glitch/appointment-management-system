4006c629d842a09c94267cd4598e4867ef1ce7a6
Fuad <kamilovfuad@gmail.com>
2022-03-31 14:31:40 +0400

    initial commit

---
+++ views/login.php
@@ -0,0 +1,23 @@
+<?php
+
+include "partials/header.php";
+
+?>
+<div class="form">
+    <div class="header">
+        <h4 class="modal-title">Log in into your account</h4>
+    </div>
+    <form action="" method="POST" class="form-horizontal">
+        <label for="username">Username</label>
+        <input type="text" name="username">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+    </form>
+</div>
+
+
+<?php
+
+include "partials/footer.php";
+
+?>
